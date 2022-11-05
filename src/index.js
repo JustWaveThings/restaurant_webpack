d23f376ac344b2eb9533f524e656f104dc2e7473
JustWaveThings <justwavethings@proton.me>
2022-11-05 17:39:37 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import _ from 'lodash';
-
-function component() {
+/* function component() {
 	const element = document.createElement('div');
 
-	// Lodash, currently included via a script, is required for this line to work
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
 	return element;
 }
 
 document.body.appendChild(component());
+ */
