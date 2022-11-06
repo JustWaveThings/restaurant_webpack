e8175d12c573127ea4b115c757fc1d6bb3c5a096
JustWaveThings <justwavethings@proton.me>
2022-11-06 14:09:35 -0500

    Move welcome code to welcome.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-/* function component() {
-	const element = document.createElement('div');
-
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-	return element;
-}
-
-document.body.appendChild(component());
- */
+import { component } from './welcome.js';
+import './style.css';
