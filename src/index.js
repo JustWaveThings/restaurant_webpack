5a18ec956e8f2889432f4adc17e27eae89d05fa6
JustWaveThings <justwavethings@proton.me>
2022-11-07 14:05:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 /* import { component as welcome } from './welcome.js'; */
-import { component as contact } from './contact.js';
+/* import { component as contact } from './contact.js'; */
+import { component as menu } from './menu.js';
 import './style.css';
