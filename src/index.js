a3f3f839dd2eabc6413e9aac10fc3d94b514cb00
JustWaveThings <justwavethings@proton.me>
2022-11-07 21:50:33 -0500

    Wrote page handling code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
-/* import { component as welcome } from './welcome.js'; */
-/* import { component as contact } from './contact.js'; */
-import { component as menu } from './menu.js';
+import { welcome } from './welcome.js';
+import { contact } from './contact.js';
+import { menuPage as menu } from './menu.js';
 import './style.css';
+
+const container2 = document.getElementById('content');
+
+const buttons = document.querySelectorAll(
+	'#content > div.footer > div > div'
+);
+
+welcome();
+refreshButtons();
+
+function refreshButtons() {
+	const buttons = document.querySelectorAll(
+		'#content > div.footer > div > div'
+	);
+
+	buttons.forEach((button) => {
+		button.addEventListener('click', (e) => {
+			managePageLoads(e);
+		});
+	});
+}
+function changePage(e) {
+	return e.target.classList[0];
+}
+
+function clearPage(cont = container2) {
+	return cont.replaceChildren();
+}
+
+function managePageLoads(e, btnClick = changePage(e)) {
+	if (btnClick === 'welcome') {
+		clearPage();
+		welcome();
+		refreshButtons();
+		console.log('show page welcome');
+	}
+	if (btnClick === 'menu') {
+		clearPage();
+		menu();
+		refreshButtons();
+		console.log('show page menu');
+	}
+	if (btnClick === 'contact') {
+		clearPage();
+		contact();
+		refreshButtons();
+		console.log('show page contact');
+	}
+}
